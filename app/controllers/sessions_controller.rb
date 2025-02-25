81a7a0a146aaa7aa9863e36fdad8a1dd35ba7b66
Marko Jelasity <marko.jelasity@kellogg.northwestern.edu>
2025-02-25 22:14:32 +0000

    Break 2

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,8 @@ class SessionsController < ApplicationController
     # 2. if the user exists -> check if they know their password
     if @user != nil #If user is not nil
     # 3. if they know their password -> login is successful
-      if @user["password"] == params["password"]
+      if BCrypt::Password.new(@user["password"]) == params["password"]
+        session["user_id"] = @user["id"]
         flash["notice"] = "Welcome!"
         redirect_to "/companies"
       else
@@ -27,6 +28,7 @@ class SessionsController < ApplicationController
   def destroy
     # logout the user
     flash["notice"] = "Goodbye."
+    session["user_id"] = nil
     redirect_to "/login"
   end
 end
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,17 +1,21 @@
 class TasksController < ApplicationController
   def index
-    @tasks = Task.all
+    @user = User.find_by({"id" => session["user_id"]})
+    @tasks = Task.where({"user_id"=> session["user_id"]})
   end
+
+
 
   def create
     @task = Task.new
     @task["description"] = params["description"]
+    @task["user_id"] = session["user_id"]
     @task.save
     redirect_to "/tasks"
   end
 
   def destroy
-    @task = Task.find_by({ "id" => params["id"] })
+    @task = Task.find_by({ "id" => params["id"], "user_id" => session["user_id"]})
     @task.destroy
     redirect_to "/tasks"
   end
